de1cd4dd8f7f96c0717a500a9e85ee5f409d7c02
Jiyong Sim <sjy19910222@gmail.com>
2022-08-04 23:18:03 +0900

    feat : 계좌 생성 API 기능 추가
    POST /account
    파라미터 : 사용자 아이디, 초기 잔액
    정책 : 사용자가 없는 경우 실패 응답,
    성공 응답 : 사용자 아이디, 계좌번호, 등록일시
    (사용자 정보가 어플리케이션 구동될 때 자동으로 저장)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into account_user(id, name, created_at, updated_at)
+values (1, 'Simjy', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (2, 'Naruto', now(), now());
+insert into account_user(id, name, created_at, updated_at)
+values (3, 'Sasuke', now(), now());
