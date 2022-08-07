4ab2287b2705497fbcf9ee856093f856915399df
Jiyong Sim <sjy19910222@gmail.com>
2022-08-07 20:33:10 +0900

    feat : 잔액 사용 취소 API 기능 추가
    - POST /transaction/cancel
    - 파라미터 : 거래 아이디, 계좌번호, 취소 요청 금액
    - 정책 : 거래 아이디에 해당하는 거래가 없는 경우, 계좌가 없는 경우, 거래와 계좌가 일치하지 않는 경우
    거래금액과 거래 취소 금액이 다른경우(부분 취소 불가능) 실패 응답
    - 1년이 넘은 거래는 사용 취소 불가능
    - 해당 계좌에서 거래(사용, 사용 취소)가 진행 중일 때
    다른 거래 요청이 오는 경우 해당 거래가 동시에 잘못 처리되는 것을 방지해야 한다.
    - 성공 응답: 계좌번호, 거래 결과 코드(성공/실패), 거래 아이디, 거래금액, 거래일

--- src/main/java/com/example/account/repository/TransactionRepository.java
+++ src/main/java/com/example/account/repository/TransactionRepository.java
@@ -10,5 +10,5 @@ import java.util.Optional;
 @Repository
 public interface TransactionRepository
         extends JpaRepository<Transaction, Long> {
-
+    Optional<Transaction> findByTransactionId(String transactionID);
 }
