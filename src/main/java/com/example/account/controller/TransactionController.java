7a6ba00809220f4958b61c0fee35410da00b7b21
Jiyong Sim <sjy19910222@gmail.com>
2022-08-08 00:27:30 +0900

    feat : 잔액 사용 확인 API 기능 추가
    - GET /transaction/{transactionId}
    - 파라미터 : 거래 아이디
    - 정책 : 해당 거래 아이디의 거래가 없는 경우 실패 응답
    - 성공 응답 : 계좌번호, 거래종류(잔액 사용, 잔액 사용 취소), 거래 결과 코드(성공/실패),
    							거래 아이디, 거래금액, 거래일시
    	- 실패한 거래(사용/사용취소)도 거래를 확인할 수 있도록 합니다.

--- src/main/java/com/example/account/controller/TransactionController.java
+++ src/main/java/com/example/account/controller/TransactionController.java
@@ -1,15 +1,14 @@
 package com.example.account.controller;
 
 import com.example.account.dto.CancelBalance;
+import com.example.account.dto.QueryTransactionReponse;
 import com.example.account.dto.TransactionDto;
 import com.example.account.dto.UseBalance;
 import com.example.account.exception.AccountException;
 import com.example.account.service.TransactionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -66,5 +65,13 @@ public class TransactionController {
 
             throw e;
         }
+    }
+
+    @GetMapping("/transaction/{transactionId}")
+    public QueryTransactionReponse queryTransaction(
+            @PathVariable String transactionId) {
+        return QueryTransactionReponse.from(
+                transactionService.queryTransaction(transactionId)
+        );
     }
 }
---
+++ src/main/java/com/example/account/dto/QueryTransactionReponse.java
@@ -0,0 +1,32 @@
+package com.example.account.dto;
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class QueryTransactionReponse {
+    private String accountNumber;
+    private TransactionType transactionType;
+    private TransactionResultType transactionResult;
+    private String transactionId;
+    private Long amount;
+    private LocalDateTime transactedAt;
+
+    public static QueryTransactionReponse from(TransactionDto transactionDto) {
+        return QueryTransactionReponse.builder()
+                .accountNumber(transactionDto.getAccountNumber())
+                .transactionType(transactionDto.getTransactionType())
+                .transactionResult(transactionDto.getTransactionResultType())
+                .transactionId(transactionDto.getTransactionId())
+                .amount(transactionDto.getAmount())
+                .transactedAt(transactionDto.getTransactedAt())
+                .build();
+    }
+}
